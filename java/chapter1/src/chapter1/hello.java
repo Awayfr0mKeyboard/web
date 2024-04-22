d2e8c23e62c28e2ad27c8b1ee379d15f1de2a465
Awayfr0mkeyboard <seohi9301@gmail.com>
2024-04-22 11:00:04 +0900

    20240422

---
+++ java/chapter1/src/chapter1/hello.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 5;
+		int z= x + y;
+		System.out.println(z);
+		
+	}
+
+}
